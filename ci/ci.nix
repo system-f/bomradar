12ed94d0cdd01fbba560390584b72cb89c7d78bd
Dave Laing <dave.laing.80@gmail.com>
2017-08-03 10:33:57 +1000

    Fixes the hydra names for the packages

--- ci/ci.nix
+++ ci/ci.nix
@@ -11,7 +11,7 @@ let
     pkgs.lib.listToAttrs (
       pkgs.lib.concatMap (compiler: 
         pkgs.lib.concatMap (system: 
-          [{name = "haskell.packages." + compiler + ".exitcode." + system ; value = {inherit compiler system;};}]
+          [{name = "haskell.packages." + compiler + ".bomradar." + system ; value = {inherit compiler system;};}]
         ) supportedSystems
       ) supportedCompilers
     );
